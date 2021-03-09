4b9b58a90ab882a2cd3dfab8092b63a9c313b42a
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-03-09 20:20:30 +0000

    user auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { auth } from './Firebase';
 
 const App = () => {
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        console.log(userAuth);
+      } else {
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <div className='app'>
       <Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyBL3dd4gNwBX9WSg_BKAyvhfwA1oSLC0ss',
   authDomain: 'netflix-build-d97be.firebaseapp.com',
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: '670081404396',
   appId: '1:670081404396:web:aa9be030ea45785d5004c9',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
