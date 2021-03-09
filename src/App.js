26b31439b8554daa57cbb427d79466c378bc8a24
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-03-09 20:38:00 +0000

    redux user setup

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { auth } from './Firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './screens/ProfileScreen';
 
 const App = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
+        dispatch(logout);
       }
     });
 
@@ -20,12 +32,18 @@ const App = () => {
   return (
     <div className='app'>
       <Router>
-        <Switch>
-          <Route exact path='/'>
-            <LoginScreen />
-            {/* <HomeScreen /> */}
-          </Route>
-        </Switch>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route path='/profile'>
+              <ProfileScreen />
+            </Route>
+            <Route exact path='/'>
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
       </Router>
     </div>
   );
---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ProfileScreen.scss';
+
+const ProfileScreen = () => {
+  return <div className='profileScreen'></div>;
+};
+
+export default ProfileScreen;
